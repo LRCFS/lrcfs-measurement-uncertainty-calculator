93192c037ecb7276a746659d1ab270e4ee4bbf40
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2019-05-23 10:49:56 +0100

    Commented out printLatexFormula and library dependancies

--- models/modelUncertaintyCalibrationCurve.R
+++ models/modelUncertaintyCalibrationCurve.R
@@ -1,5 +1,5 @@
-library(latex2exp)
-library(stringr)
+# library(latex2exp)
+# library(stringr)
 
 ################################
 ## Fixed properties
@@ -227,44 +227,44 @@ getRelativeStandardUncertainty = function(x,y,input){
   return(relativeStandardUncertainty)
 }
 
-printLatexFormula = function(latex, calc, variables){
-  
-  # latex = "z = $$x$$ * $$y$$"
-  # calc = "$$x$$ * $$y$$";
-  # variables = data.frame("\\sum\\limits_{i=1}^n(y_i-\\hat{y}_i)^2" = 5, "$$n-2$$" = 3)
-  
-  latexOutput = latex
-  for(colName in names(variables))
-  {
-    regexString = paste0("(\\$\\$",colName,"\\$\\$)");
-    latexOutput = str_replace_all(latexOutput, regexString, colName)
-  }
-  latexOutput = paste0("$$",latexOutput,"$$")
-  
-  latexWithSubs = latex
-  for(colName in names(variables))
-  {
-    regexString = paste0("(\\$\\$",colName,"\\$\\$)");
-    latexWithSubs = str_replace_all(latexWithSubs, regexString, as.character(variables[,colName]))
-    print(colName)
-  }
-  latexWithSubs = paste0("$$",latexWithSubs,"$$")
-  
-  calcSubs = calc
-  for(colName in names(variables))
-  {
-    regexString = paste0("(\\$\\$",colName,"\\$\\$)");
-    calcSubs = str_replace_all(calcSubs, regexString, as.character(variables[,colName]))
-  }
-  calcSubs
-
-  answer = eval(parse(text=calcSubs))
-  answer
-  
-  output = data.frame("latex" = latexOutput, "latexWithSubs" = latexWithSubs, "answer" = answer)
-    
-  return(output)
-}
+# printLatexFormula = function(latex, calc, variables){
+#   
+#   # latex = "z = $$x$$ * $$y$$"
+#   # calc = "$$x$$ * $$y$$";
+#   # variables = data.frame("\\sum\\limits_{i=1}^n(y_i-\\hat{y}_i)^2" = 5, "$$n-2$$" = 3)
+#   
+#   latexOutput = latex
+#   for(colName in names(variables))
+#   {
+#     regexString = paste0("(\\$\\$",colName,"\\$\\$)");
+#     latexOutput = str_replace_all(latexOutput, regexString, colName)
+#   }
+#   latexOutput = paste0("$$",latexOutput,"$$")
+#   
+#   latexWithSubs = latex
+#   for(colName in names(variables))
+#   {
+#     regexString = paste0("(\\$\\$",colName,"\\$\\$)");
+#     latexWithSubs = str_replace_all(latexWithSubs, regexString, as.character(variables[,colName]))
+#     print(colName)
+#   }
+#   latexWithSubs = paste0("$$",latexWithSubs,"$$")
+#   
+#   calcSubs = calc
+#   for(colName in names(variables))
+#   {
+#     regexString = paste0("(\\$\\$",colName,"\\$\\$)");
+#     calcSubs = str_replace_all(calcSubs, regexString, as.character(variables[,colName]))
+#   }
+#   calcSubs
+# 
+#   answer = eval(parse(text=calcSubs))
+#   answer
+#   
+#   output = data.frame("latex" = latexOutput, "latexWithSubs" = latexWithSubs, "answer" = answer)
+#     
+#   return(output)
+# }
 
 
 
