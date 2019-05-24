eccd4f5e5122db87a0a9bfd75f41f059a4d4bcff
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2019-05-24 10:28:19 +0100

    Update viewDashboard.R

--- views/viewDashboard.R
+++ views/viewDashboard.R
@@ -1,6 +1,6 @@
 tabDashboard = tabItem(tabName = "dashboard",
                        fluidRow(
-                         valueBox("Uncertainty of Calibration Curve Joyce Test", uiOutput("dashboardUncertaintyOfCalibrationCurve"), width = 12, color = "blue", icon = icon("chart-line")),
+                         valueBox("Uncertainty of Calibration Curve", uiOutput("dashboardUncertaintyOfCalibrationCurve"), width = 12, color = "blue", icon = icon("chart-line")),
                          valueBox("Uncertainty of Quality Control", "\\(u_r\\text{(QulControl)}=\\)", width = 6, color = "red", icon = icon("dashboard")),
                          valueBox("Uncertainty of Standard Solution", "\\(u_r\\text{(StdSolution)}=\\)", width = 6, color = "green", icon = icon("vial")),
                          valueBox("Combined Uncertainty", "\\(u_r\\text{(CombUncertainty)}=\\)", width = 6, color = "purple", icon = icon("gitter")),
