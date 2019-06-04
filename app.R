8d3ec47ca2f03539b13e61821eccc339bafae4a0
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2019-06-04 10:23:37 +0100

    Retabbed file

--- app.R
+++ app.R
@@ -29,41 +29,41 @@ source("views/viewExpandedUncertainty.R")
 `%ni%` = Negate(`%in%`)
 
 ui <- dashboardPagePlus(title="METEOR v0.3",
-  dashboardHeaderPlus(title = tagList(
-    img(class = "logo-lg", src = "images/logo-large.png"), 
-    img(class = "logo-mini", src = "images/logo-small.png")),
-                      enable_rightsidebar = TRUE,
-                      rightSidebarIcon = "gears"
-                     ),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Results Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Calibration Curve", tabName = "calibrationCurve", icon = icon("chart-line")),
-      menuItem("Method Precision", tabName = "methodPrecision", icon = icon("bullseye")),
-      menuItem("Standard Solution", tabName = "standardSolution", icon = icon("vial")),
-      menuItem("Combined Uncertainty", tabName = "combinedUncertainty", icon = icon("arrows-alt-v")),
-      menuItem("Expanded Uncertainty", tabName = "expandedUncertainty", icon = icon("arrows-alt"))
-    )
-  ),
-  dashboardBody(
-    tags$head(tags$link(rel = "shortcut icon", href = "images/favicon.ico")),
-    #Use MathJax for rendering inline LaTeX functions
-    withMathJax(),
-    #Load tabs from view files (Properties can be found in /views/view...R)
-    tabItems(
-      tabDashboard,
-      tabCalibrationCurve,
-      tabMethodPrecision,
-      tabStandardSolution,
-      tabCombinedUncertainty,
-      tabExpandedUncertainty
-    )
-  ),
-  rightsidebar = mouCalcRightSidebar,
-  footer = dashboardFooter(
-    left_text = "METEOR",
-    right_text = "Copyright LRCFS 2019"
-  )
+                        dashboardHeaderPlus(title = tagList(
+                          img(class = "logo-lg", src = "images/logo-large.png"), 
+                          img(class = "logo-mini", src = "images/logo-small.png")),
+                          enable_rightsidebar = TRUE,
+                          rightSidebarIcon = "gears"
+                        ),
+                        dashboardSidebar(
+                          sidebarMenu(
+                            menuItem("Results Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+                            menuItem("Calibration Curve", tabName = "calibrationCurve", icon = icon("chart-line")),
+                            menuItem("Method Precision", tabName = "methodPrecision", icon = icon("bullseye")),
+                            menuItem("Standard Solution", tabName = "standardSolution", icon = icon("vial")),
+                            menuItem("Combined Uncertainty", tabName = "combinedUncertainty", icon = icon("arrows-alt-v")),
+                            menuItem("Expanded Uncertainty", tabName = "expandedUncertainty", icon = icon("arrows-alt"))
+                          )
+                        ),
+                        dashboardBody(
+                          tags$head(tags$link(rel = "shortcut icon", href = "images/favicon.ico")),
+                          #Use MathJax for rendering inline LaTeX functions
+                          withMathJax(),
+                          #Load tabs from view files (Properties can be found in /views/view...R)
+                          tabItems(
+                            tabDashboard,
+                            tabCalibrationCurve,
+                            tabMethodPrecision,
+                            tabStandardSolution,
+                            tabCombinedUncertainty,
+                            tabExpandedUncertainty
+                          )
+                        ),
+                        rightsidebar = mouCalcRightSidebar,
+                        footer = dashboardFooter(
+                          left_text = "METEOR",
+                          right_text = "Copyright LRCFS 2019"
+                        )
 )
 
 server <- function(input, output) {
