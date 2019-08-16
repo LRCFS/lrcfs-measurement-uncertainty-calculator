e5f0a9035679d3521c4ef10550fee7e0a358a86a
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2019-08-16 10:15:50 +0100

    Updated title bar name

--- app.R
+++ app.R
@@ -43,7 +43,7 @@ source("views/viewDashboard.R")
 
 `%ni%` = Negate(`%in%`)
 
-ui <- dashboardPagePlus(title="METEOR v0.5",
+ui <- dashboardPagePlus(title="MOU Calc v0.5",
                         dashboardHeaderPlus(title = tagList(
                           img(class = "logo-lg", src = "images/logo-large.png"), 
                           img(class = "logo-mini", src = "images/logo-small.png")),
