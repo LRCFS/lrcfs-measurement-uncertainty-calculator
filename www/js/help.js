440d52d1bf2bc9b0014f3638975eb21be386b98e
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2019-09-19 17:08:59 +0100

    Update help.js
    Corrected typo

--- www/js/help.js
+++ www/js/help.js
@@ -21,7 +21,7 @@ function(startingStep) {
 			intro: '<h4>Calibration Curve</h4>\
 					<p>Calibration curve data is data on concentration levels and peak area (ratios) used to generate the calibration for estimating the level of concentration in a given new sample.</p>\
 					<h4>External Standard Error</h4>\
-					<p>In place of the standard error of regression, standard error from previous calibration curve data can be pooled together with current calibration data to obtain a more reliable estimate. This is recommended if calibration curve data uploaded above have only one replicate or is based on simple point calibration system.</p>',
+					<p>In place of the standard error of regression, standard error from previous calibration curve data can be pooled together with current calibration data to obtain a more reliable estimate. This is recommended if calibration curve data uploaded above have only one replicate or is based on single point calibration system.</p>',
 			position: "right",
 			width: "500px"
 		},
