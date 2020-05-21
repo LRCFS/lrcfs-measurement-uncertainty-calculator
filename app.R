5b6271cd851ab842ad358c5b6468ffe770932703
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2020-05-21 10:40:10 +0100

    Added packrat versioning of packages

--- app.R
+++ app.R
@@ -5,40 +5,27 @@
 #Clear all lists from memory to avoid unintentional errors
 rm(list = ls())
 
-#Install packages if needed
-#ref: https://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
-needPackages = function(...) {
-  requiredLibraries = unlist(list(...)); #Get the list of required libraries from the method parameters
-  requireResults = unlist(lapply(requiredLibraries,require,character.only=TRUE)); #Run the "require" method of each package and save the result (using the character.only flag allows you to search by passing in a variable)
-  missingLibraries = requiredLibraries[requireResults==FALSE]; #Store the missing libraries where the results of the "require" method are false 
-  if(length(missingLibraries > 0)) #If we have any missing libraries
-  {
-    install.packages(missingLibraries); #Install the packages that are missing
-    lapply(missingLibraries,library,character.only=TRUE) #Once installed then run the library function to register them
-  }
-}
-
-needPackages( "shiny",
-              "shinyjs",
-              "shinydashboard",
-              "shinydashboardPlus",
-              "ggplot2",
-              "reshape2",
-              "scales",
-              "dplyr",
-              "plotly",
-              "DT",
-              "DiagrammeR",
-              "stringr",
-              "data.tree",
-              "rintrojs",
-              "textutils",
-              "tinytex",
-              "rmarkdown",
-              "knitr",
-              "webshot",
-              "shinyWidgets",
-              "colourpicker")
+library(shiny)
+library(shinyjs)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(dplyr)
+library(plotly)
+library(DT)
+library(DiagrammeR)
+library(stringr)
+library(data.tree)
+library(rintrojs)
+library(textutils)
+library(tinytex)
+library(rmarkdown)
+library(knitr)
+library(webshot)
+library(shinyWidgets)
+library(colourpicker)
 
 source("models/modelHelperFunctions.R")
 
