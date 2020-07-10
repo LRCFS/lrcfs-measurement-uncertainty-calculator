cd066b2c01f9818bf25224949f4ff6242a092365
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2020-07-10 14:28:35 +0100

    Added copyright

--- README.md
+++ README.md
@@ -34,3 +34,23 @@ RStudio has built in support for Shiny and because we've just installed it (if y
 > Close R Studio and open app.R
 
 Now that we've installed all the required dependancies, and loaded the app.R in RStudio, you will see an option to "Run App" at the top right of the app.R source code.
+
+# Measurement Uncertainty Calculator - Copyright (C) 2019
+## Leverhulme Research Centre for Forensic Science
+## Roy Mudie, Joyce Klu, Niamh Nic Daeid
+#### Website: https://github.com/LRCFS/lrcfs-measurement-uncertainty-calculator/
+#### Contact: lrc@dundee.ac.uk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
--- models/modelStart.R
+++ models/modelStart.R
@@ -1,3 +1,26 @@
+###########################################################################
+#
+# Measurement Uncertainty Calculator - Copyright (C) 2019
+# Leverhulme Research Centre for Forensic Science
+# Roy Mudie, Joyce Klu, Niamh Nic Daeid
+# Website: https://github.com/LRCFS/lrcfs-measurement-uncertainty-calculator/
+# Contact: lrc@dundee.ac.uk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###########################################################################
+
 ###################################################################################
 # Outputs
 ###################################################################################
