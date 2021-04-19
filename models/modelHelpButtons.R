fb210fba9cccb5fa1c3643085fa979eb5f48ce25
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2021-04-19 11:23:29 +0100

    Fixed bug with help buttons not starting on correct link

--- models/modelHelpButtons.R
+++ models/modelHelpButtons.R
@@ -28,36 +28,12 @@ observeEvent(input$help_start_start, {
   session$sendCustomMessage(type = 'runjs_help_start', message = 1)
 })
 
-observeEvent(input$help_start_homogeneity, {
+observeEvent(input$help_start_caseSampleData, {
   session$sendCustomMessage(type = 'runjs_help_start', message = 2)
 })
 
-observeEvent(input$help_start_calcurve, {
-  session$sendCustomMessage(type = 'runjs_help_start', message = 3)
-})
-
-observeEvent(input$help_start_methodprec, {
-  session$sendCustomMessage(type = 'runjs_help_start', message = 4)
-})
-
-observeEvent(input$help_start_stdsol, {
-  session$sendCustomMessage(type = 'runjs_help_start', message = 5)
-})
-
-observeEvent(input$help_start_sampleprep, {
-  session$sendCustomMessage(type = 'runjs_help_start', message = 6)
-})
-
-observeEvent(input$help_start_weightedLeastSquare, {
-  session$sendCustomMessage(type = 'runjs_help_start', message = 7)
-})
-
-observeEvent(input$help_start_caseSampleData, {
-  session$sendCustomMessage(type = 'runjs_help_start', message = 8)
-})
-
 observeEvent(input$help_start_confidenceInterval, {
-  session$sendCustomMessage(type = 'runjs_help_start', message = 9)
+  session$sendCustomMessage(type = 'runjs_help_start', message = 3)
 })
 
 
