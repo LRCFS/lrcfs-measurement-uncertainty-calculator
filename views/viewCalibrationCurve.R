6d3bba46b4c7caab6cd7f6a0c7be84df34d16552
Roy Mudie <5470814+RoyMudie@users.noreply.github.com>
2021-04-26 12:52:50 +0100

    Updated report

--- views/viewCalibrationCurve.R
+++ views/viewCalibrationCurve.R
@@ -29,7 +29,7 @@ tabCalibrationCurve = tabItem(tabName = "calibrationCurve",
                  fluidRow(
                    box(title = "Overview", width=6,
                        p("All computations and details of formulas used for computing the uncertainty of the calibration curve are displayed here. This version of the software assumes a linear calibration curve with the option to specify weights if weighted least squares (WLS) regression is required."),
-                       p("The Method tab shows the main formulas used to compute the uncertainty of the calibration curve for both weighted and non-weighted least squares regression. All calculations preformed in this application use the WLS formula which is equivilent to a simple linear regression (non-weighted) formula if the weight \\(W=1\\) is specified.")
+                       p("The Method tab shows the main formulas used to compute the uncertainty of the calibration curve for both weighted and non-weighted least squares regression. All calculations preformed in this application use the WLS formula which is equivalent to a simple linear regression (non-weighted) formula if the weight \\(W=1\\) is specified.")
                    ),
                    tabBox(title = "Method", width=6,
                           tabPanel("Weighted",
@@ -37,7 +37,7 @@ tabCalibrationCurve = tabItem(tabName = "calibrationCurve",
                                    "$$u\\text{(CalCurve)} = \\frac{S_{w}}{b_1} \\sqrt{\\frac{1}{w_{s}(r_s)} + \\frac{1}{n} + \\frac{(x_s - \\overline{x}_w)^2}{S_{{xx}_w}} }$$",
                                    "where \\(S_w\\) is the standard error of regression given by",
                                    "$$S_w = \\sqrt{\\frac{\\sum\\limits_{i=1}^n w_i(y_i-\\hat{y}_i)^2}{n-2}}$$",
-                                   "The Realtive Standard Uncertainty is then calculated as:",
+                                   "The Relative Standard Uncertainty is then calculated as:",
                                    "$$ u_r\\text{(CalCurve)} = \\frac{\\text{Standard Uncertatiny}}{\\text{Case Sample Mean Concentration}} = \\frac{u\\text{(CalCurve)}}{x_s} $$",
                                    tags$ul(
                                      tags$li("\\(x_i\\) concentration at level \\(i\\)."),
@@ -56,11 +56,11 @@ tabCalibrationCurve = tabItem(tabName = "calibrationCurve",
                                    )
                           ),
                           tabPanel("Non-Weighted",
-                                   "When a wieght of \\(W = 1\\) is specified the uncertainty is simplified to the following:",
+                                   "When a weight of \\(W = 1\\) is specified the uncertainty is simplified to the following:",
                                    "$$u\\text{(CalCurve)} = \\frac{S_{y/x}}{b_1} \\sqrt{\\frac{1}{r_s} + \\frac{1}{n} + \\frac{(x_s - \\overline{x})^2}{S_{xx}}}$$",
                                    "where \\(S_{y/x}\\) is the standard error of regression given by",
                                    "$$S_{y/x} = \\sqrt{\\frac{\\sum\\limits_{i=1}^n(y_i-\\hat{y}_i)^2}{n-2}}$$",
-                                   "The Realtive Standard Uncertainty is then calculated as:",
+                                   "The Relative Standard Uncertainty is then calculated as:",
                                    "$$ u_r\\text{(CalCurve)} = \\frac{\\text{Standard Uncertatiny}}{\\text{Case Sample Mean Concentration}} = \\frac{u\\text{(CalCurve)}}{x_s} $$",
                                    tags$ul(
                                      tags$li("\\(x_i\\) concentration at level \\(i\\)."),
